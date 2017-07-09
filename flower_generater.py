5d1de800720ae4c20c2280e5deeb34dc59dd514f
hanoia <ykochi@outlook.jp>
2017-07-09 12:50:36 +0900

    Update generate petal
    花びらの種類を増やした

--- flower_generater.py
+++ flower_generater.py
@@ -5,14 +5,17 @@ import uuid
 
 
 class Petal:
-    def __init__(self, path):
+    def __init__(self, path, rotate):
         self.scale_ratio = 0.4
         self.shift_ratio = 1.1
         self.brightness = 1.8
+        self.rotate = rotate
         self.petal_image_path = path
 
     def generate_petal(self, width, color):
-        petal_gray = Image.open(self.petal_image_path).convert('LA')
+        petal_gray = Image.open(self.petal_image_path).convert('LA').rotate(self.rotate, expand=1)
+        crop = petal_gray.split()[-1].getbbox()
+        petal_gray.crop(crop)
         brightness_converter = ImageEnhance.Brightness(petal_gray)
         petal_base = brightness_converter.enhance(self.brightness)
         petal_source = petal_base.split()
@@ -57,13 +60,18 @@ class Flower:
 
         return canvas
 
-def generate_file(color, number):
-    petal = Petal('petal0.png')
+def generate_file(color, number, petal_type):
+    petals = [
+        Petal('petal0.png', 0),
+        Petal('petal1.png', -55)
+    ]
+    petal = petals[petal_type]
     flower = Flower(petal)
     img = flower.generate(color, number)
     filename = str(uuid.uuid4()) + '.png'
     img.save(filename)
     return filename
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate flower image.')
@@ -71,6 +79,8 @@ if __name__ == "__main__":
                         default=[128,128,128], type=int, help = 'color of petals')
     parser.add_argument('-n', '--number', default=6, type=int,
                         help='number of petals')
+    parser.add_argument('-t', '--type', default=0, type=int,
+                        help='shape type of petal')
     args = parser.parse_args()
 
-    print(generate_file(args.color, args.number))
+    print(generate_file(args.color, args.number, args.type))
